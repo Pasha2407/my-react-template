751265f0a925995e31be0b2f5a56a3c9a531c868
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2024-11-07 18:07:24 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from 'App';
+import { App } from 'components/App/App';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/my-react-template">
